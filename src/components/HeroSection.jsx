bbad4ed647d35ee8bbb82dff97c44356d972d489
JullianMQ <jullianmq@gmail.com>
2025-01-17 16:34:54 +0800

    changed layout in desktop and added icons

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ const HeroSection = () => {
     <>
       <section id="home" className="mx-2 h-full md:h-auto">
         <TopSection />
-        <section className="my-8 xl:hidden">
+        <section className="md:mb-20 md:mt-8 xl:hidden">
           <BottomSection />
         </section>
       </section>
@@ -14,7 +14,7 @@ const HeroSection = () => {
 const TopSection = () => {
   return (
     <section className="top-section">
-      <div className="xl:grid-rows-4 mx-2 my-8 grid grid-cols-8 grid-rows-2 gap-2 md:grid-rows-1">
+      <div className="mx-2 mt-8 grid grid-cols-8 grid-rows-2 gap-2 md:grid-rows-1 xl:grid-rows-4">
 
         <div className="col-span-2 col-start-1 col-end-5 row-span-2 row-start-1 
           overflow-hidden rounded-xl border border-white transition-all duration-200 
@@ -26,59 +26,62 @@ const TopSection = () => {
             src="/my_pic.webp" alt="my picture" />
         </div>
 
-        <div className="mt-8 hidden xl:col-span-4 xl:col-start-4 xl:row-span-3 
-          xl:row-start-1 xl:block xl:ml-8 2xl:ml-12">
-          <BottomSection />
-        </div>
-
+        {/* LOGOS */}
         <div className="md:-col-start-4 col-start-5 col-end-7 row-start-1 
           my-4 flex items-end justify-center rounded-xl text-white lg:row-start-1 
-          lg:items-start xl:col-span-1 xl:col-start-4 xl:col-end-4 xl:row-start-3 
-          xl:mb-0 xl:mt-10 xl:items-end 2xl:row-start-2 2xl:row-end-4 2xl:items-center">
+          lg:items-start xl:col-span-1 xl:col-start-4 xl:col-end-4 xl:row-start-1 
+          xl:mb-0 xl:mt-10 xl:items-end 2xl:row-start-1 2xl:row-end-2 2xl:items-center">
 
-          <a href="#" className="flex h-12 w-12 items-center justify-center 
-            rounded-full border-2 border-blue-500 bg-gray-200 text-gray-800 
+          <a href="https://www.facebook.com/JulzMQ" target="_blank" className="flex h-12 w-12 items-center justify-center 
+            rounded-full border-2 bg-gray-200 text-gray-800 
             transition-colors duration-300 hover:bg-gray-400 md:h-24 md:w-24 ">
-
-            FB
+            <img src="src/assets/facebook-logo.svg" alt="facebook logo" height="200" width="200" />
           </a>
         </div>
 
         <div className="md:-col-start-3 col-start-6 col-end-9 row-start-1 my-4 flex 
           items-end justify-center rounded-xl text-white lg:row-start-1 lg:items-start
-          xl:col-span-1 xl:col-start-5 xl:row-start-3 xl:mb-0 xl:mt-10 xl:items-end
-          2xl:row-start-2 2xl:row-end-4 2xl:items-center">
+          xl:col-span-1 xl:col-start-5 xl:row-start-1 xl:mb-0 xl:mt-10 xl:items-end
+          2xl:row-start-1 2xl:row-end-2 2xl:items-center">
 
-          <a href="#" className="flex h-12 w-12 items-center justify-center 
-            rounded-full border-2 border-blue-500 bg-gray-200 text-gray-800
+          <a href="https://www.instagram.com/julz.m.l/" target="_blank" className="flex h-12 w-12 items-center justify-center 
+            rounded-full border-2 bg-gray-200 text-gray-800
             transition-colors duration-300 hover:bg-gray-400 md:h-24 md:w-24 ">
-            IG
+            <img src="src/assets/instagram-logo.svg" alt="instagram logo" height="200" width="200" />          
           </a>
         </div>
 
         <div className="md:-col-start-4 col-start-5 col-end-7 row-start-2 
           my-4 flex items-start justify-center rounded-xl text-white
-          lg:row-start-1 lg:items-end xl:col-span-1 xl:col-start-6 xl:row-start-3
-          xl:mb-0 xl:mt-10 xl:items-end 2xl:row-start-2 2xl:row-end-4 2xl:items-center">
+          lg:row-start-1 lg:items-end xl:col-span-1 xl:col-start-6 xl:row-start-1
+          xl:mb-0 xl:mt-10 xl:items-end 2xl:row-start-1 2xl:row-end-2 2xl:items-center">
 
-          <a href="#" className="flex h-12 w-12 items-center justify-center 
-            rounded-full border-2 border-blue-500 bg-gray-200 text-gray-800
+          <a href="https://github.com/JullianMQ" target="_blank" className="flex h-12 w-12 items-center justify-center 
+            rounded-full border-2 bg-gray-200 text-gray-800
             transition-colors duration-300 hover:bg-gray-400 md:h-24 md:w-24 ">
-            GH
+            <img src="src/assets/github-logo.svg" alt="github logo" height="200" width="200" />          
           </a>
         </div>
 
         <div className="md:-col-start-3 col-start-6 col-end-9 row-start-2 my-4
           flex items-start justify-center rounded-xl text-white lg:row-start-1
-          lg:items-end xl:col-span-1 xl:col-start-7 xl:row-start-3 xl:mb-0
-          xl:mt-10 xl:items-end 2xl:row-start-2 2xl:row-end-4 2xl:items-center">
+          lg:items-end xl:col-span-1 xl:col-start-7 xl:row-start-1 xl:mb-0
+          xl:mt-10 xl:items-end 2xl:row-start-1 2xl:row-end-2 2xl:items-center">
 
-          <a href="#" className="flex h-12 w-12 items-center justify-center 
-            rounded-full border-2 border-blue-500 bg-gray-200 text-gray-800
+          <a href="https://www.linkedin.com/in/jullian-quiambao-28142a2a2/" target="_blank" className="flex h-12 w-12 items-center justify-center 
+            rounded-full border-2 bg-gray-200 text-gray-800
             transition-colors duration-300 hover:bg-gray-400 md:h-24 md:w-24 ">
-            LK
+            <img src="src/assets/linkedin-logo.svg" alt="linkedin logo" height="200" width="200" />          
           </a>
         </div>
+        {/* LOGOS */}
+
+        {/* INTRODUCTION */}
+        <div className="mt-8 hidden xl:col-span-4 xl:col-start-4 xl:row-span-4 
+          xl:row-start-2 xl:ml-8 xl:block 2xl:ml-12">
+          <BottomSection />
+        </div>
+        {/* INTRODUCTION */}
 
       </div>
     </section>
@@ -90,19 +93,22 @@ const BottomSection = () => {
     <>
       <div className="mb-12 ml-2 mr-2 grid grid-cols-8 grid-rows-4">
         <h1 className="col-span-8 row-span-1 self-end text-3xl font-semibold 
-          uppercase leading-3 text-gray-100 md:col-start-1 md:text-5xl lg:col-start-2
-          xl:col-start-1">
+          uppercase leading-3 text-gray-100 md:col-start-1 md:text-5xl
+          md:row-start-1 lg:col-start-2 xl:col-start-1 xl:row-start-1 ">
           Jullian Quiambao
         </h1>
 
-        <h1 className="col-span-8 row-span-1 text-2xl font-semibold uppercase 
-          text-gray-100 md:col-start-1 md:text-5xl lg:col-start-2 xl:col-start-1">
+        <h1 className="col-span-8 col-start-1 row-span-1 row-start-2 text-2xl 
+          font-semibold uppercase text-gray-100 md:col-start-1 md:text-5xl
+          md:row-start-1 md:row-span-2 md:mt-10 lg:col-start-2 xl:col-start-1
+          xl:row-span-2 xl:row-start-1 xl:mt-12">
           Full Stack Developer
         </h1>
 
-        <p className="col-span-8 row-span-2 self-start text-lg leading-5 
-          tracking-normal text-gray-100 md:col-start-1 md:text-2xl md:leading-7 
-          lg:col-start-2 xl:col-start-1">
+        <p className="col-span-8 col-start-1 row-span-3 row-start-2 mt-12 
+          self-start text-lg leading-5 tracking-normal text-gray-100
+          md:col-start-1 md:text-2xl md:leading-6 lg:col-start-2 xl:col-start-1
+          xl:mt-12 xl:leading-6">
           A full stack developer that builds functional websites without sacrificing beauty. I thrive in high-pressure environments, delivering exceptional results while continously seeking ways to grow and master new skills. I would <b>love</b> to work with you.
         </p>
       </div>
