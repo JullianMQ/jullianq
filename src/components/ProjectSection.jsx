86d55a52cee215d1a4b4dcf332130bdcf8d03ef2
JullianMQ <jullianmq@gmail.com>
2025-01-17 16:35:20 +0800

    added props and icons to component

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -4,29 +4,36 @@ const ProjectSection = () => {
   return (
     <section id="projects" className="mx-2 flex h-auto flex-col items-center gap-8">
 
-      <ProjectCard 
+      <ProjectCard
         title="Akaru"
-        description="Akaru is a digital library that allows readers to read in a way that is engaging, and that is easy and fun. Akaru aims to bridge a gap between the reading experience and ease of use."
+        role="Lead Backend Developer"
+        description="Akaru is a digital library that allows readers to read in a way that is engaging, is easy and fun. Akaru aims to bridge a gap between the reading experience and ease of use."
         image="src/assets/index.png"
+        languages={["src/assets/javascript-logo.svg", "src/assets/firebase-logo.svg", "src/assets/express-logo.svg"]}
       />
-      <ProjectCard 
-        title="Akaru"
-        description="Akaru is a digital library that allows readers to read in a way that is engaging, and that is easy and fun. Akaru aims to bridge a gap between the reading experience and ease of use."
-        image="src/assets/index.png"
+      <ProjectCard
+        title="Kalium"
+        role="Frontend Developer"
+        description="Kalium is a project that showcases the design and development of a user-friendly website for Kalium Drugstore's pharmacy services. It highlights skills in web development, UI/UX design, and e-commerce functionality to enhance customer engagement and streamline access to pharmaceutical products."
+        image="src/assets/kalium-hero.png"
+        languages={["src/assets/javascript-logo.svg", "src/assets/tailwind-logo.svg", "src/assets/mongodb-logo.svg"]}
       />
-      <ProjectCard 
-        title="Akaru"
-        description="Akaru is a digital library that allows readers to read in a way that is engaging, and that is easy and fun. Akaru aims to bridge a gap between the reading experience and ease of use."
-        image="src/assets/index.png"
+      <ProjectCard
+        title="Medica"
+        role="PM/Lead Backend"
+        description="Medica Pharmacy is a student project that aims to create a user-friendly website that streamlines the process of ordering medication from a pharmacy. The website is designed to be visually appealing, easy to navigate, and provides a seamless shopping experience for customers."
+        image="src/assets/medica.png"
+        languages={["src/assets/javascript-logo.svg", "src/assets/mysql-logo.svg", "src/assets/php-logo.svg"]}
       />
     </section>
   )
 }
 
-const ProjectCard = ({ image, title, description }) => {
+const ProjectCard = ({ image, title, role, description, languages, links }) => {
   const project_title = title
   const project_image = image
   const project_description = description
+  const project_languages = languages
 
   return (
     <motion.div className="grid-rows-8 grid w-full grid-cols-6 md:gap-2 
@@ -38,14 +45,30 @@ const ProjectCard = ({ image, title, description }) => {
 
       <div className="col-span-1 row-span-3 space-y-2 justify-self-center 
         xl:col-start-2 xl:mr-8 xl:place-self-end">
-        <div className="h-10 w-10 rounded-full border-2 border-white"> </div>
-        <div className="h-10 w-10 rounded-full border-2 border-white"> </div>
-        <div className="h-10 w-10 rounded-full border-2 border-white"> </div>
+        {project_languages.map((language, index) => {
+          return (
+            <div
+              key={index}
+              className="group h-10 w-10 rounded-full border-2 border-white 
+             hover:border-gray-400 transition-colors duration-300"
+            >
+              <div className="h-full w-full bg-transparent group-hover:bg-black/50
+                transition-colors duration-300 rounded-full">
+                <img
+                  src={language}
+                  alt="language logo"
+                  className="h-full w-full object-contain group-hover:opacity-50
+                  transition-opacity duration-300"
+                />
+              </div>
+            </div>
+          )
+        })}
       </div>
 
-      <div className="col-span-4 row-span-3 flex items-center justify-center border
-      border-white text-white md:row-span-4 xl:col-span-3 xl:row-span-5">
-        <img src={project_image} alt="akaru image" />
+      <div className="col-span-4 row-span-3 flex justify-center 
+      text-white md:row-span-4 xl:col-span-3 xl:row-span-5">
+        <img className="object-cover aspect-auto" src={project_image} alt="akaru image" />
       </div>
 
       <div className="col-span-1 row-span-3 space-y-2 justify-self-center 
@@ -55,14 +78,21 @@ const ProjectCard = ({ image, title, description }) => {
         <div className="h-12 w-12 rounded-full border-2 border-white"> </div>
       </div>
 
-      <div className="projects-padding col-span-full row-start-5 mx-2
-        font-bold md:col-span-4 md:row-start-5 lg:col-span-3 xl:col-span-4
-        xl:col-start-6 xl:col-end-9 xl:row-start-1 xl:flex xl:items-center">
-        <h1 className="text-4xl text-white xl:text-3xl 2xl:text-4xl">{project_title}</h1>
+      <div className="projects-padding flex items-center col-start-1 col-span-full
+        row-start-5 mx-2 font-bold md:col-span-full md:row-start-5 md:col-start-1 
+        xl:col-span-4 xl:col-start-6 xl:col-end-9 xl:row-start-1 xl:flex
+        xl:items-center">
+        <h1 className="text-4xl text-white xl:text-3xl 2xl:text-4xl">
+          {project_title}
+        </h1>
+        <p className="text-neutral-400 ml-8"
+          style={{ fontFamily: "Staatliches" }}
+        >{role}</p>
       </div>
 
-      <div className="projects-padding col-span-full row-span-3 row-start-6 mx-2
-        md:row-start-6 xl:col-start-6 xl:col-end-12 xl:row-start-2 xl:mt-2">
+      <div className="projects-padding col-start-1 col-span-full row-span-3
+        row-start-5 mx-2 md:col-start-1 md:col-end-7 mt-16 
+        xl:col-start-6 xl:col-end-12 xl:row-start-2 xl:mt-2">
         <p className="text-white">
           {project_description}
         </p>
